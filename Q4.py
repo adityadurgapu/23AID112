eeded28f18480cd2d26fff801e42fc6e126ac3b2
Aditya Durgapu <adityadurgapu@gmail.com>
2025-12-24 20:46:23 +0530

    Deciding the variable even or odd

---
+++ Q4.py
@@ -0,0 +1,6 @@
+#ASKING NUMBER AND DECIDING IT IS EVENN OR ODD.
+fav_num = int(input("Enter your favourite number :"))
+if fav_num % 2 == 0:
+    print("Your favourite number is an even number")
+else :
+    print("Your favourite number is an odd number")
