0a17754a399d6f538f5bfbd2b4e74f48accad024
Aditya Durgapu <adityadurgapu@gmail.com>
2025-12-25 12:14:46 +0530

    Deciding the number +ve,-ve or 0

---
+++ Q7.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number:"))
+if (num == 0):
+    print("The entered number is Zero")
+elif (num < 0):
+    print("The entered number is negative")
+else :
+    print("The entered number is positive")
