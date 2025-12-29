630d631d8e065be42b5ba86169babea620ef14b7
Aditya Durgapu <adityadurgapu@gmail.com>
2025-12-29 22:38:06 +0530

    Age calculator

---
+++ week-01/Q2.py
@@ -0,0 +1,6 @@
+# Calculating the age in year 2025. 
+Age = input("Enter your year of birth:")# It is a String.
+# Convert it into an Integer.
+Age = int(Age)
+years = 2025 - Age
+print("You will be",years,"years old by 2025")
