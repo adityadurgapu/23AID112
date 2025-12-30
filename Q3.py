62b458961ac7c6ef9c1894a5bd5990a3bbf4bbaf
Aditya Durgapu <adityadurgapu@gmail.com>
2025-12-30 18:53:11 +0530

    Discount

--- Q3.py
+++
@@ -1,6 +0,0 @@
-#Assigning the variables.
-#Float
-price = 599.99
-discount = 0.15
-price_after_discount =price-(price*discount)
-print("The price afer the discount is: %.2f"%price_after_discount)#%.2f rounds off the value to decimal points
