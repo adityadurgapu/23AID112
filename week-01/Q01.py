9d40dd5a37911dedf43650939d2d40ce28739d92
Aditya Durgapu <adityadurgapu@gmail.com>
2026-01-01 11:06:25 +0530

    Data Types

--- week-01/Q01.py
+++ week-01/Q01.py
@@ -2,13 +2,13 @@
 name = (input("Enter  your name:"))
 age = int(input("Enter your age:"))
 height = float(input("Enter your height:"))
-verify = (input("are you a student? (yes/no):"))
+verify = (input("are you a student? (true/false):"))
 
 print("Your name is:",name)
 print("Your height is:",height)
 print("Your age is:",age)
 
-if verify == "yes":
+if (verify):
     print(name,"is a student")
 else:
     print(name,"is not a student")
