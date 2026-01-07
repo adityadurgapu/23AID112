ad17c03da9a8575bfcc52de53bd51ed79daf5dfd
Aditya Durgapu <adityadurgapu@gmail.com>
2026-01-07 21:12:56 +0530

    enumerates operation

---
+++ week-01/enumerates.py
@@ -0,0 +1,11 @@
+list = {
+
+    'laptop ': 152110,
+    'mouse':5165,
+    'keyboard':818,
+    'cooler':685
+}
+
+for index,product in enumerate(list,1):
+    print(index,product)
+  #  print(product)
