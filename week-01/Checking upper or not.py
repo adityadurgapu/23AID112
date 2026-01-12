5c26db588fc5fb2c67961b29df1c1e0c4b5c2be1
Aditya Durgapu <adityadurgapu@gmail.com>
2026-01-12 20:13:11 +0530

    checking upper or not

---
+++ week-01/Checking upper or not.py
@@ -0,0 +1,7 @@
+string = input("Enter your string:")
+if string.isupper():# .ISUPPER() CHECKS IT IS UPPER OR NOT.
+    print("upper")
+else:
+    print("not upper!")
+
+#IN SAME METHOD WE CAN CHECK .ISLOWER() ALSO.
